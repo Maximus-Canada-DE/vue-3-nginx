9a891ca344432855b33e8cc77028a22f41ce4ecc
Jonathan Langlois <jonathan.langlois@maximuscanada.ca>
2022-06-08 12:13:12 -0700

    test image publish

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,7 +15,7 @@ defineProps({
       You’ve successfully created a project with
       <a target="_blank" href="https://vitejs.dev/">Vite</a> +
       <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
-      without trigger
+      testing
     </h3>
   </div>
 </template>
